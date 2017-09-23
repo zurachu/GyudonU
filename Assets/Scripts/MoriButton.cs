a57d7c36aae3f85a123cfbdeda750bbeed88d894
zurachu <zurachu@gmail.com>
2017-09-23 11:13:42 +0900

    「○盛」ボタンを押したら牛丼が出てきてドラッグできるようにした。
    Canvas内で場所を合わせるのに親子関係を外したり付け直したりしないとうまくいっておらず、もっとスマートなやり方があるのではないかと思われる。

---
+++ Assets/Scripts/MoriButton.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MoriButton : MonoBehaviour
+	, IPointerDownHandler
+	, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public enum MoriSize
+	{
+		Namimori,
+		Oomori,
+		Tokumori,
+	};
+	public MoriSize moriSize;
+
+	private GameObject canvas;
+	private GameObject gyudonPrefab;
+	private GameObject gyudonInstance;
+
+	// Use this for initialization
+	void Start () {
+		canvas = GameObject.Find("Canvas");
+		gyudonPrefab = Resources.Load<GameObject>("Prefabs/Gyudon");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void SetGyudonPosition(PointerEventData eventData)
+	{
+		gyudonInstance.transform.SetParent(canvas.transform.parent, false);
+		Vector2 localPosition = Vector2.zero;
+		RectTransformUtility.ScreenPointToLocalPointInRectangle(
+			canvas.GetComponent<RectTransform>(), eventData.position, Camera.main, out localPosition);
+		gyudonInstance.GetComponent<RectTransform>().position = localPosition;
+		gyudonInstance.transform.SetParent(canvas.transform, false);
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		gyudonInstance = Instantiate(gyudonPrefab);
+		SetGyudonPosition(eventData);
+	}
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		Debug.Log(eventData.position);
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		SetGyudonPosition(eventData);
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+
+	}
+}

bc8a6a75939037a7311ac31e0e3e840c21040bcc
zurachu <zurachu@gmail.com>
2017-09-27 20:59:59 +0900

    客をプレハブ化して、ひとまず起動時に全席に生成するようにした。

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	private GameObject canvas;
+	private GameObject customerPrefab;
+
+	private const float flickSpeedRate = 5;
+
+	// Use this for initialization
+	void Start()
+	{
+		canvas = GameObject.Find("Canvas");
+		customerPrefab = Resources.Load<GameObject>("Prefabs/Customer");
+        for (int i = 0; i < 6; i++)
+        {
+            AddCustomer(i);
+        }
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+    private void AddCustomer(int index)
+    {
+		var instance = Instantiate(customerPrefab);
+        var rect = instance.GetComponent<RectTransform>();
+        var position = rect.transform.position;
+        var width = rect.sizeDelta.x;
+        position.x += width * index;
+        rect.transform.position = position;
+		instance.transform.SetParent(canvas.transform, false);
+	}
+}

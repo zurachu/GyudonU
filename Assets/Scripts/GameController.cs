7264a13340ddd25204b9aaa47077383268f12ec8
zurachu <zurachu@gmail.com>
2017-09-29 10:17:57 +0900

    客を空席に新規に登場させる処理を追加。

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+    private const int NumChair = 6;
+
 	private GameObject canvas;
 	private GameObject customerPrefab;
+    private GameObject[] customer = new GameObject[NumChair];
 
 	// Use this for initialization
 	void Start()
 	{
 		canvas = GameObject.Find("Canvas");
 		customerPrefab = Resources.Load<GameObject>("Prefabs/Customer");
-        for (int i = 0; i < 6; i++)
-        {
-            AddCustomer(i);
-        }
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-
+        if(Random.Range(0, 2000) < 100)
+        {
+            int index = Random.Range(0, NumChair);
+            if (!customer[index])
+            {
+				AddCustomer(index);
+			}
+        }
 	}
 
     private void AddCustomer(int index)
@@ -33,5 +39,6 @@ public class GameController : MonoBehaviour {
         position.x += width * index;
         rect.transform.position = position;
 		instance.transform.SetParent(canvas.transform, false);
+        customer[index] = instance;
 	}
 }

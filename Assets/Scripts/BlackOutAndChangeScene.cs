bf98b7fba5c61401f5f6d48c60a37495231838db
zurachu <zurachu@gmail.com>
2017-10-08 00:19:01 +0900

    メインメニューのシーンを仮に作成し、ボタン押下でインゲームに飛ぶようにした。

---
+++ Assets/Scripts/BlackOutAndChangeScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class BlackOutAndChangeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var image = GetComponent<Image>();
+        var color = image.color;
+        color.a += Time.deltaTime;
+        image.color = color;
+        if (color.a >= 1)
+        {
+            SceneManager.LoadScene("InGame");
+        }
+	}
+}
